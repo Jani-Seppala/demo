32788d80747989bbf6871cad9eba55a1a2731e64
Jani-Seppala <jani.seppala6@gmail.com>
2020-08-17 12:09:36 +0300

    hienosäädetty koodia ja lisätty käyttöliittymään osallistuminen veikkaukseen

--- tietokantaharjoittelu3.py
+++ tietokantaharjoittelu3.py
@@ -1,8 +1,10 @@
 import sqlite3
 
 """
-SELVITÄ miksi otteluiden tuloksien muokkaus ei aktivoi pelattu switchiä oikein
-SELVITÄ miksi switch ei toimi muutenkaan, liittynevät samaan asiaan
+SELVITÄ miksi laske_yhteispisteet() pitää ajaa 2 kertaa jotta muutokset tulevat näkyviin konsoliin vaikka muutokset tulevat voimaan databaseen jo ekalla ajolla
+auttaako IS NULL?
+RATKAISE paivita_tulos() ottelutulosten päivitys niin että laske_yhteispisteet() laskee kaikki ottelut jotka on pelattu, eikä lopeta laskemista noneen
+TEE nickin uniikkiudelle tarkastus databasesta ja tarkista veikkaukset tutki_syote() funktion kautta
 """
 
 def alusta_tietokanta():
@@ -16,14 +18,14 @@ cur = conn.cursor()
 cur.executescript("""CREATE TABLE IF NOT EXISTS tulokset(
                         tulos_id INTEGER PRIMARY KEY,
                         ottelupari text,
-                        tulos text NULL,
-                        pelattu text NULL                        
+                        tulos text DEFAULT NULL,
+                        pelattu text DEFAULT NULL                        
             );
 
             CREATE TABLE IF NOT EXISTS osallistujat(
                         osallistuja_id INTEGER PRIMARY KEY,
                         osallistuja text UNIQUE,
-                        pisteet integer
+                        pisteet integer DEFAULT 0
 
             );
             CREATE TABLE IF NOT EXISTS veikkaukset(
@@ -38,16 +40,18 @@ cur.executescript("""CREATE TABLE IF NOT EXISTS tulokset(
 
 
 # Ottelu_lista = [('Rus-Sau'), ('Egt-Uru'), ('Mor-Ira'), ('Por-Spa'), ('Fra-Aus'), ('Arg-Ice'), ('Per-Den'), ('Cro-Nig'), ('Cos-Ser'), ('Ger-Mex')]
-# Ottelu_lista = [('Rus-Sau', None), ('Egt-Uru', None), ('Mor-Ira', None), ('Por-Spa', None), ('Fra-Aus', None), ('Arg-Ice', None), ('Per-Den', None), ('Cro-Nig', None), ('Cos-Ser', None), ('Ger-Mex', None)]
 Ottelu_lista = [('Rus-Sau', '5-0'), ('Egt-Uru', '0-1'), ('Mor-Ira', '0-1'), ('Por-Spa', '3-3'), ('Fra-Aus', None),
                 ('Arg-Ice', None), ('Per-Den', None), ('Cro-Nig', None), ('Cos-Ser', None), ('Ger-Mex', None)]
-Osallistujat_lista = [('Kingis', 0), ('Matti', 0), ('Jussi', 0)]
+# Ottelu_lista = [('Rus-Sau', '5-0'), ('Egt-Uru', '0-1'), ('Mor-Ira', '0-1'), ('Por-Spa', '3-3'), ('Fra-Aus', ),
+#                 ('Arg-Ice', ), ('Per-Den', ), ('Cro-Nig', ), ('Cos-Ser', ), ('Ger-Mex', )]
+
+Osallistujat_lista = [('Kingis', ), ('Matti', ), ('Jussi', )]
 Veikkaukset_lista = [(1, 1, '2-0'), (1, 2, '1-2'), (1, 3, '1-1'), (1, 4, '1-2'), (1, 5, '1-1'),
                      (2, 1, '2-0'), (2, 2, '1-3'), (2, 3, '1-1'), (2, 4, '2-1'), (2, 5, '2-0'),
                      (3, 1, '1-1'), (3, 2, '1-2'), (3, 3, '2-1'), (3, 4, '2-2'), (3, 5, '3-0')]
 
 cur.executemany("INSERT INTO tulokset(ottelupari, tulos) VALUES (?, ?)", Ottelu_lista)
-cur.executemany("INSERT INTO osallistujat(osallistuja, pisteet) VALUES (?, ?)", Osallistujat_lista)
+cur.executemany("INSERT INTO osallistujat(osallistuja) VALUES (?)", Osallistujat_lista)
 cur.executemany("INSERT INTO veikkaukset(osallistuja_id, tulos_id, veikkaus) VALUES (?, ?, ?)", Veikkaukset_lista)
 conn.commit()
 
@@ -113,9 +117,11 @@ def paivita_tulos():
         if tulos_id in id_lista:
             paivita_tulos_lista = tarkista_syote()
 
-            cur.execute("UPDATE tulokset SET tulos = ('{}') WHERE tulos_id = ('{}')".format(paivita_tulos_lista, tulos_id))
-            cur.execute("UPDATE tulokset SET pelattu = ('{}') WHERE tulos_id = ('{}')".format(0, tulos_id))
+            sql_update_tulos_query = "UPDATE tulokset SET tulos = ? WHERE tulos_id = ?"
+            tulos_data = (paivita_tulos_lista, tulos_id)
+            cur.execute(sql_update_tulos_query, tulos_data)
             conn.commit()
+
             print(f"Ottelu id {tulos_id} päivitetty tuloksella {paivita_tulos_lista}")
 
         elif tulos_id == "":
@@ -134,7 +140,7 @@ def tarkista_syote():
         vierasmaalit = input('Vierasjoukkueen maalit:').strip()
 
         if kotimaalit.isdigit() and vierasmaalit.isdigit():
-            if (int(kotimaalit) < 99 or int(kotimaalit) > 0) and (int(vierasmaalit) < 99 or int(vierasmaalit) > 0):
+            if 0 <= int(kotimaalit) <= 99 and 0 <= int(vierasmaalit) <= 99:
                 tulos_str = f"{kotimaalit}-{vierasmaalit}"
                 return tulos_str
             else:
@@ -145,41 +151,48 @@ def tarkista_syote():
 
 def laske_yhteispisteet():
 
+    tulokset = cur.execute("SELECT * FROM tulokset")
+    tulokset = tulokset.fetchall()
+
+    veikkaukset = cur.execute("SELECT * FROM veikkaukset")
+    veikkaukset = veikkaukset.fetchall()
+
     for tulosrivi in tulokset:
         for veikkausrivi in veikkaukset:
             # JOS ottelun id == osallistujan veikkaaman ottelun id
             if tulosrivi[0] == veikkausrivi[2]:
                 print("------------")
-                osallistujat_dict = {1: 'Kingis', 2: 'Matti', 3: 'Jussi'}
                 tulos_id = tulosrivi[0]
                 tulos = tulosrivi[2]
                 pelattu_switch = tulosrivi[3]
                 veikkaajan_id = veikkausrivi[1]
                 veikkaus = veikkausrivi[3]
 
-                print(f"{osallistujat_dict[veikkaajan_id]} veikkaus {veikkaus}, tulos {tulos}")
+                sql_osallistuja_nimi_query = "SELECT osallistuja FROM osallistujat WHERE osallistuja_id = ?"
+                data = (veikkaajan_id, )
+                sql_exe = cur.execute(sql_osallistuja_nimi_query, data)
+                sql_exe = sql_exe.fetchone()
+
+                print(f"{sql_exe} veikkaus {veikkaus} otteluun {tulosrivi[1]}, tulos {tulos}")
 
                 if not tulos:
                     continue
 
                 elif not pelattu_switch:
-                    cur.execute("UPDATE tulokset SET pelattu = ('{}') WHERE tulos_id = ('{}')".format(1, tulos_id))
-                    conn.commit()
+                    sql_update_pelattu_query = "UPDATE tulokset SET pelattu = ? WHERE tulos_id = ?"
+                    pelattu_data = (True, tulos_id)
+                    cur.execute(sql_update_pelattu_query, pelattu_data)
 
                     pisteet = laske_ottelu_pisteet(tulos, veikkaus)
 
                     if pisteet > 0:
-                        print(f"lisätään {pisteet} pistettä osallistuja id:lle {osallistujat_dict[veikkaajan_id]}")
 
-                        # HAE tämän hetkisen osallistujan pistetiedot, sijoita ne vanhat_pisteet muuttujaan, lisää siihen uudet pisteet
-                        cur.execute("SELECT pisteet FROM osallistujat WHERE osallistuja_id = ('{}')".format(veikkaajan_id))
-                        vanhat_pisteet = cur.fetchone()
-                        pisteet = vanhat_pisteet[0] + pisteet
+                        print(f"lisätään {pisteet} pistettä osallistuja id:lle {sql_exe}")
 
-                        # SIJOITA uudelleen lasketut pisteet takaisin tietokantaan saman osallistujan pistetietoihin
-                        cur.execute(
-                            "UPDATE osallistujat SET pisteet = ('{}') WHERE osallistuja_id = ('{}')".format(pisteet,
-                                                                                                            veikkaajan_id))
+                        # HAE tämän hetkisen osallistujan pistetiedot ja päivitä ne
+                        sql_paivita_pisteet_query = "UPDATE osallistujat SET pisteet = pisteet + ? WHERE osallistuja_id = ?"
+                        paivita_pisteet_data = (pisteet, veikkaajan_id)
+                        cur.execute(sql_paivita_pisteet_query, paivita_pisteet_data)
                         conn.commit()
 
                     else:
@@ -191,12 +204,50 @@ def laske_yhteispisteet():
     cur.execute("SELECT * FROM osallistujat")
     print(cur.fetchall())
 
+    cur.execute("SELECT * FROM tulokset")
+    print(tulokset)
 
 
 
 def pistetilanne():
     cur.execute("SELECT * FROM osallistujat")
     print(cur.fetchall())
+
+
+def luo_osallistuja():
+
+    luotu_nimi = input("Anna nimi/nick jolla osallistut kisaan: ")
+    sql_luo_osallistuja_query = "INSERT INTO osallistujat(osallistuja) VALUES (?)"
+    osallistuja_data = (luotu_nimi, )
+
+    cur.execute(sql_luo_osallistuja_query, osallistuja_data)
+    conn.commit()
+
+    sql_valitse_osallistuja_query = "SELECT osallistuja_id FROM osallistujat WHERE osallistuja = ?"
+    cur.execute(sql_valitse_osallistuja_query, osallistuja_data)
+    uusi_osallistuja_id = cur.fetchone()
+
+    luo_veikkaus(uusi_osallistuja_id[0])
+
+
+def luo_veikkaus(uusi_osallistuja_id):
+
+    sql_hae_tulokset_query = "SELECT tulos_id, ottelupari FROM tulokset"
+    cur.execute(sql_hae_tulokset_query)
+    data = cur.fetchall()
+
+    osallistujan_veikkaukset = []
+    for ottelu in data:
+        ottelupari = ottelu[1]
+        otteluid = ottelu[0]
+        print(ottelu[1])
+        osallistujan_veikkaus = input(f"Anna veikkaus ottelulle {ottelupari}: ")
+
+        osallistujan_veikkaukset.append((uusi_osallistuja_id, otteluid, osallistujan_veikkaus))
+
+    sql_tallenna_veikkaukset_query = "INSERT INTO veikkaukset(osallistuja_id, tulos_id, veikkaus) VALUES (?, ?, ?)"
+    # veikkaus_data = (osallistujan_veikkaukset, )
+    cur.executemany(sql_tallenna_veikkaukset_query, osallistujan_veikkaukset)
 
 
 def main():
@@ -217,6 +268,8 @@ def main():
             alusta_tietokanta()
         elif syote == 'l':
             laske_yhteispisteet()
+        elif syote == 'o':
+            luo_osallistuja()
         elif syote == 'p':
             pistetilanne()
         elif syote == 'm':
